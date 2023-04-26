c47e8734b5ab8aee0711fe6abd04ff42e27d2b6c
VitaliyHraboviak <117772319+VitaliyHraboviak@users.noreply.github.com>
2023-04-26 17:24:55 +0000

    Deploying to gh-pages from @ VitaliyHraboviak/goit-js-hw-11@489480ef41c09fc361772d20698c1c89fec01338 🚀

--- index.580cd40a.js
+++
@@ -1,2 +0,0 @@
-fetch("https://pixabay.com/api/?key=35556462-a8d5565fa0c9d2dfbbf17af0f&q=yellow+flowers&image_type=photo").then((function(e){return response.json()})).then((function(e){console.log(e)}));
-//# sourceMappingURL=index.580cd40a.js.map
